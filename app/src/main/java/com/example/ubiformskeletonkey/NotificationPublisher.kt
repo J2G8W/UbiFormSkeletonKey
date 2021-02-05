a5f8f2336a1e296eda3c7defd2cf9857c64dee1a
Julian <julian@ashleywreford.com>
2021-02-05 13:57:42 +0000

    Potential fix for notification errors

--- app/src/main/java/com/example/ubiformskeletonkey/NotificationPublisher.kt
+++ app/src/main/java/com/example/ubiformskeletonkey/NotificationPublisher.kt
@@ -7,11 +7,13 @@ import android.content.Context
 import android.content.Intent
 import android.content.ServiceConnection
 import android.graphics.Bitmap
+import android.os.Build
 import android.os.IBinder
 import android.service.notification.NotificationListenerService
 import android.service.notification.StatusBarNotification
 import android.util.Base64
 import android.util.Log
+import androidx.annotation.RequiresApi
 import androidx.core.graphics.drawable.toBitmap
 import java.io.ByteArrayOutputStream
 
@@ -37,17 +39,21 @@ class NotificationPublisher : NotificationListenerService() {
         if (ubiformServiceBound) {
             if (sbn != null) {
                 var byteArray: ByteArray? = null
+                if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+                    val remotePackageContext = applicationContext.createPackageContext(
+                            sbn.notification.smallIcon.resPackage,0)
 
-                val icon = sbn.notification.smallIcon.loadDrawable(this)
-                if (icon != null) {
-                    val bitmap = icon.toBitmap(
-                        icon.intrinsicWidth,
-                        icon.intrinsicHeight,
-                        Bitmap.Config.ARGB_8888
-                    )
-                    val stream = ByteArrayOutputStream()
-                    bitmap.compress(Bitmap.CompressFormat.PNG, 100, stream)
-                    byteArray = Base64.encode(stream.toByteArray(), Base64.NO_WRAP)
+                    val icon = sbn.notification.smallIcon.loadDrawable(remotePackageContext)
+                    if (icon != null) {
+                        val bitmap = icon.toBitmap(
+                            icon.intrinsicWidth,
+                            icon.intrinsicHeight,
+                            Bitmap.Config.ARGB_8888
+                        )
+                        val stream = ByteArrayOutputStream()
+                        bitmap.compress(Bitmap.CompressFormat.PNG, 100, stream)
+                        byteArray = Base64.encode(stream.toByteArray(), Base64.NO_WRAP)
+                    }
                 }
 
                 ubiFormService.publishNotification(
